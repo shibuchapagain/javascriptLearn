4ba90dae9d01435acde2631461765080004c5163
shiva chapagain <shibuchapagain12@gmail.com>
2023-05-12 21:19:27 +0545

    upload day1

---
+++ day1/script.js
@@ -0,0 +1,18 @@
+// logical operator:
+
+let num1 = 40; // false
+let num2 = 50; // false
+
+// truthy // falsy
+
+// && --> num1 () && num2;
+
+// && (t, t, t)=> t
+
+let check = num1 && num2;
+console.log(check, "test1"); // 5, 55 ==>
+
+// || (or opera)
+
+// 1) trial (pass license)
+// likhich -> trial
---
+++ day1/script2.js
@@ -0,0 +1,149 @@
+// // OR OPERATOR:
+// // (||) symbol:
+
+// // js keyword: let, const, var, for
+
+// // truthy , false:
+
+// const num1 = 5; // truth
+// const num2 = 6; // truth
+
+// let check = num1 || num2; // or -> truth -> return, , && -> move,
+// console.log(check, "test4"); // 6->
+
+// // truth table:
+// // or -> t || t-> t
+// // t || f -> t
+// // f || f-> f
+// // f || t -> t // false || true -> t
+
+// num1 || num2 ? console.log(true) : console.log(false);
+// // console.log(isValid, "test1"); // true
+
+// console.log(check, "test2"); // 6 ,
+
+// NOT OPERATOR:
+// true -> false, false ->  true
+
+// let isValid = false;
+// console.log(!isValid);
+
+// ->
+
+// DATE:
+// console.log(new Date().now());
+
+// let current = new Date();
+// let secondVariable = Date.now();
+// console.log(secondVariable);
+// console.log(current);
+
+// current -> second
+// expiryDate-> second +120;
+
+// CONDITIONAL:
+
+// if, else
+// if else
+
+// if else:
+// let age = 20;
+
+// // if (age > 34) {
+// //   console.log("apply for license");
+// // } else {
+// //   console.log("fail");
+// // }
+
+// // ternary operator:
+// age > 18 ? console.log("pass") : console.log("fail");
+
+// PROMPT:
+// let bornYear = prompt("enter your born year"); // -> return string -> 2000
+// console.log(+bornYear + 2); // 62 -> 2002
+// const calcAge = 2023 - bornYear;
+// console.log(calcAge);
+// console.log(typeof userAge);
+// console.log(userAge);
+
+// if else if->
+
+// let number = 24;
+
+// if (number < 20) {
+//   // false
+//   console.log("below 20"); // not console
+// } else if (number < 30) {
+//   // true
+//   // false
+//   console.log("under 30");
+// } else {
+//   console.log("pass");
+// }
+
+// let currentDay = prompt("enter today");
+// console.log(currentDay, "test");
+
+// switch (currentDay) {
+//   case "sunday":
+//     console.log("its sunday");
+//     break;
+//   case "monday":
+//     console.log("its monday");
+//     break;
+//   default:
+//     console.log("wrong input");
+// }
+
+// new Date(); --> node
+// date --> moment --> a day ago, 5 minutes day,
+// new Date();
+
+// LOOP:
+// certain ->
+// for (let i = 4; i <= 6; i++) { // increment:
+//   // recurvice
+//   // i = 4,
+//   console.log(i, "test");
+// }
+// for loop:
+// for (let i=; i++)
+// let number = 5;
+// console.log(number++); // return 5
+// console.log(number); // 6
+// console.log(++number); // 6
+
+// for of, for in ->
+
+// for of -> number
+/// for in -> index
+
+let numbers = [11, 22, 33, 44, 55]; // number -> array, start with 0;
+// console.log(numbers[0]); // 0-> index;
+// // console.log(numbers.length);
+
+// i -> index;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   //   console.log(i); // index ->each number index
+//   console.log(numbers[i]);
+// }
+
+// let output = [];
+// console.log(output, "test1");
+
+// for (let num of numbers) {
+//   // numbers->
+//   //   console.log(num); // value
+//   output.push(num);
+// }
+
+// console.log(output, "test2");
+
+// FOR IN LOOP:
+// for (let index in numbers) {
+//   //   console.log(index);
+// //   console.log(numbers[index]);
+// }
+
+// BANkist -> APP (array, -> )
